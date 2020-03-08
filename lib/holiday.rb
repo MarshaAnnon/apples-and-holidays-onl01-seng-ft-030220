ca649e321eaa16f04e5c655bba7ecc124785d3e6
Marsha Annon <marshaannon@gmail.com>
2020-03-08 23:35:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,14 +47,21 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring].each do |holiday, supplies|
     if holiday == :memorial_day
       supplies << supply
-      binding.pry
     end
   end
 end
+end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[:spring].each do |holiday, supplies|
+    if holiday == :memorial_day 
+      supplies << supply
+    end
+  end
+end
 
 end
 
