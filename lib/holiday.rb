c4410eb59efb745f7faa2334d7f7752dd121f3f1
Marsha Annon <marshaannon@gmail.com>
2020-03-09 00:20:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_winter_holiday_supplies(holiday_hash)
   array = holiday_hash[:winter].collect do |holiday, items|
     items
   end
-  array
+  array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
