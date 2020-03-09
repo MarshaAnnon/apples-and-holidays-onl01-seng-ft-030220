41af723c54967f4cd336b5cab30bf05eda805f99
Marsha Annon <marshaannon@gmail.com>
2020-03-09 00:42:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,8 +88,8 @@ def all_holidays_with_bbq(holiday_hash)
 
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if supplies.include?('BBQ')
-        puts "#{holiday}:"
+      if holiday == ('BBQ')
+        puts "#{holiday}"
       end
     end
   end
